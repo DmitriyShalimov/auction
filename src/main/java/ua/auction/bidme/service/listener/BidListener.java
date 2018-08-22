92140ba9362dbd8cb4e297ef0e109c90c268b611
Dmitriy Shalimov <dmitriyshalimov@gmail.com>
2018-08-22 17:34:32 +0300

    add string format fpr message

--- src/main/java/ua/auction/bidme/service/listener/BidListener.java
+++ src/main/java/ua/auction/bidme/service/listener/BidListener.java
@@ -3,6 +3,7 @@ package ua.auction.bidme.service.listener;
 import ua.auction.bidme.dao.MessageDao;
 import ua.auction.bidme.entity.Lot;
 import ua.auction.bidme.entity.Message;
+import ua.auction.bidme.entity.SuccessIndicator;
 import ua.auction.bidme.entity.User;
 
 import java.time.LocalDateTime;
@@ -14,6 +15,9 @@ import static ua.auction.bidme.entity.SuccessIndicator.getById;
 public class BidListener {
     private Map<String, User> bidLeader = new HashMap<>();
     private MessageDao messageDao;
+    private  final static String  LOWER_BIB_MESSAGE="Someone made a higher bid for %s%n";
+    private  final static String  HIGHER_BIB_MESSAGE="Your bid for %s%n are leading now. Congratulations!";
+    private  final static String  WIN_MESSAGE="You are win lot %s%n. Congratulations!";
 
     public BidListener(MessageDao messageDao) {
         this.messageDao = messageDao;
@@ -22,17 +26,17 @@ public class BidListener {
     public void notify(int lotId, String lotTitle, User user) {
         User secondBidUser = bidLeader.get(String.valueOf(lotId));
         if (secondBidUser != null) {
-            messageDao.add(createNewMessage(lotId, secondBidUser.getId(), "F", "Someone made a higher bid for " + lotTitle));
+            messageDao.add(createNewMessage(lotId, secondBidUser.getId(), SuccessIndicator.FAIL.getId(), String.format(LOWER_BIB_MESSAGE,lotTitle)));
         }
-        messageDao.add(createNewMessage(lotId, user.getId(), "S", "Your bid for " + lotTitle + " are leading now. Congratulations!"));
+        messageDao.add(createNewMessage(lotId, user.getId(), SuccessIndicator.SUCCESS.getId(), String.format(HIGHER_BIB_MESSAGE,lotTitle) ));
         bidLeader.put(String.valueOf(lotId), user);
     }
 
     public void notifyWinner(Lot lot) {
-        String text = "You are win lot " + lot.getTitle() + ". Congratulations!";
+        String text = String.format(WIN_MESSAGE,lot.getTitle());
         User winner = bidLeader.get(String.valueOf(lot.getId()));
         if (winner != null) {
-            messageDao.add(createNewMessage(lot.getId(), bidLeader.get(String.valueOf(lot.getId())).getId(), "S", text));
+            messageDao.add(createNewMessage(lot.getId(), bidLeader.get(String.valueOf(lot.getId())).getId(), SuccessIndicator.SUCCESS.getId(), text));
         }
     }
 
