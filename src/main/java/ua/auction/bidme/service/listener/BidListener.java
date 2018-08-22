08c183d0c115bcb61ee2e29c4670faf67df9ac7f
chickitosik <chickitosik@gmail.com>
2018-08-22 18:53:50 +0300

    user page. add message date to ui
    - add thymeleaf extrass java8 time dependecy to format message date.

--- src/main/java/ua/auction/bidme/service/listener/BidListener.java
+++ src/main/java/ua/auction/bidme/service/listener/BidListener.java
@@ -10,7 +10,8 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-import static ua.auction.bidme.entity.SuccessIndicator.getById;
+import static java.lang.String.format;
+import static ua.auction.bidme.entity.SuccessIndicator.*;
 
 public class BidListener {
     private Map<Integer, User> bidLeader = new HashMap<>();
@@ -26,23 +27,23 @@ public class BidListener {
     public void notify(int lotId, String lotTitle, User user) {
         User secondBidUser = bidLeader.get(lotId);
         if (secondBidUser != null) {
-            messageDao.add(createNewMessage(lotId, secondBidUser.getId(), SuccessIndicator.FAIL.getId(), String.format(LOWER_BID_MESSAGE, lotTitle)));
+            messageDao.add(createMessage(lotId, secondBidUser.getId(), FAIL, format(LOWER_BID_MESSAGE, lotTitle)));
         }
-        messageDao.add(createNewMessage(lotId, user.getId(), SuccessIndicator.SUCCESS.getId(), String.format(HIGHER_BID_MESSAGE, lotTitle)));
+        messageDao.add(createMessage(lotId, user.getId(), SUCCESS, format(HIGHER_BID_MESSAGE, lotTitle)));
         bidLeader.put(lotId, user);
     }
 
     public void notifyWinner(Lot lot) {
-        String text = String.format(WIN_MESSAGE, lot.getTitle());
+        String text = format(WIN_MESSAGE, lot.getTitle());
         User winner = bidLeader.get(lot.getId());
         if (winner != null) {
-            messageDao.add(createNewMessage(lot.getId(), bidLeader.get(lot.getId()).getId(), SuccessIndicator.SUCCESS.getId(), text));
+            messageDao.add(createMessage(lot.getId(), bidLeader.get(lot.getId()).getId(), SUCCESS, text));
         }
     }
 
-    private Message createNewMessage(int lotId, int userId, String indicator, String text) {
+    private Message createMessage(int lotId, int userId, SuccessIndicator indicator, String text) {
         return new Message.Builder(text)
-                .indicator(getById(indicator))
+                .indicator(indicator)
                 .dateTime(LocalDateTime.now())
                 .userId(userId)
                 .lotId(lotId)
